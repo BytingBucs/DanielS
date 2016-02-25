f1b13e9931112e83c1a8e4dee23b8c474e75724a
DEScott <dscott10@bucs.fsw.edu>
2016-02-25 16:02:02 -0500

    Complete JSON example, should probably merge branch now.

--- chapter5/externaljsonexample/javascript/app.js
+++ chapter5/externaljsonexample/javascript/app.js
@@ -1,8 +1,25 @@
 var main = function () {
 	"use strict";
 	
+	console.log("Hello world");
+	
 	$.getJSON("cards/aceOfSpades.json", function (card) {
-		console.log(card);
+		var $cardParagraph = $("<p>");
+		
+		$cardParagraph.text(card.rank + " of " + card.suit);
+		
+		$("main").append($cardParagraph);
+	});
+	
+	$.getJSON("cards/hand.json", function (hand) {
+		var $list = $("<ul>");
+		hand.forEach(function (card) {
+			var $card = $("<li>");
+			$card.text(card.rank + " of " + card.suit);
+			$list.append($card);
+		});
+		
+		$("main").append($list);
 	});
 };
 
