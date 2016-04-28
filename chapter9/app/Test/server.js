b2e4192ffd6cbc4ae29e3e5e9be3549b4d8e131d
DEScott <dscott10@bucs.fsw.edu>
2016-04-28 07:41:10 -0400

    I found out why mongoose wasn't working, I simply didn't run the install command. Now this directory has what it needs to run it but I don't think this app uses it...

--- chapter9/app/Test/server.js
+++ chapter9/app/Test/server.js
@@ -1,21 +1,21 @@
 var express = require("express"),
 	http = require("http"),
-	//mongoose = require("mongoose"),
+	mongoose = require("mongoose"),
 	bodyParser = require("body-parser"),
 	app = express(),
 	services;
-	//mongoUrl = "mongodb://localhost/Test";
+	mongoUrl = "mongodb://localhost/Test";
 
 app.use(express.static(__dirname + "/client"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-/* if (process.env.VCAP_SERVICES) {
+if (process.env.VCAP_SERVICES) {
 	services = JSON.parse(process.env.VCAP_SERVICES);
 	mongoUrl = services["mongolab"][0].credentials.uri;
 }
 
-mongoose.connect(mongoUrl); */
+mongoose.connect(mongoUrl);
 
 http.createServer(app).listen(process.env.PORT || 3000);
 
